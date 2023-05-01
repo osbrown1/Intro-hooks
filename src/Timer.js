a9f26f70f555a76e81c57ddd8e098a97a66b2998
osbrown1 <socknot@live.com>
2023-05-01 10:41:56 -0700

    update

---
+++ src/Timer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import useTimer from './hooks/useTimer';
+
+function Timer() {
+  // New line below!
+  const [isActive, timer, setIsActive] = useTimer();
+
+  return (
+    <React.Fragment>
+      {isActive ? <h1>{timer}</h1> : <h1>Timer Stopped</h1>}
+      <button onClick={() => setIsActive(!isActive)}>Start/Stop</button>
+    </React.Fragment>
+  );
+}
+
+export default Timer;
---
+++ src/hooks/useTimer.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function useTimer() {
+  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer(timerState => timerState + 1)
+      }, 1000
+    )}
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  // New line below!
+  return [isActive, timer, setIsActive]; 
+}
+
+export default useTimer;
